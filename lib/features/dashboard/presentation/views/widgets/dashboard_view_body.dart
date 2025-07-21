0e1cf2df0d669225036a8ce479632d3a37cab5af
EsAm1973 <me354172@gmail.com>
2025-07-21 13:47:15 +0300

    Create Dashboard View

--- lib/features/dashboard/presentation/views/widgets/dashboard_view_body.dart
+++ lib/features/dashboard/presentation/views/widgets/dashboard_view_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_market_dashboard/core/widgets/custom_buttom.dart';
 
 class DashboardViewBody extends StatelessWidget {
   const DashboardViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [CustomButtom(onpressed: () {}, text: 'Add Data')],
+      ),
+    );
   }
 }
